c55cd840f70121318f24a76d76223b48ffb54ec6
Josh Forner <fornerjos@gmail.com>
2020-05-12 11:50:24 -0400

    formatting

--- PointOfSaleMidTerm/ProductListView.cs
+++ PointOfSaleMidTerm/ProductListView.cs
@@ -15,7 +15,7 @@ namespace PointOfSaleMidTerm
             for (int i = 0; i < this.Products.Count; i++)
             {
                 Product p = this.Products[i];
-                Console.WriteLine($"\t{i,-3}|{p.Name,-15}|{p.Category,-8}|{p.Description,-30}|${p.Price}");
+                Console.WriteLine($"\t{i,-3}|{p.Name,-16}|{p.Category,-8}|{p.Description,-31}|${p.Price}");
             }
         }
     }
